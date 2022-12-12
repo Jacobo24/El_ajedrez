69fb84f021b58b374f7a76854a1c6ba1a231449f
Jacobo24 <jaccalgar@gmail.com>
2022-12-12 18:32:57 +0100

    tablero

--- ajedrez.py
+++ ajedrez.py
@@ -6,3 +6,10 @@ ajedrez=[   ["♜"   "♞"    "♝"    "♛"	"♚"   "♝"    "♞"    "♜"],
             ["  "   "  "    "  "    "  "    "  "   "  "   "  "    "  "],
             ["♙"   "♙"    "♙"    "♙"	"♙"    "♙"   "  "    "♙"],
             ["♖"   "♘"    "♗"    "♕"	"♔"    "♗"    "♘"   "♖"]]
+
+# Para que se vea el tablero de ajedrez
+def imprimir_tablero(ajedrez):
+    for i in range(8):
+        for j in range(8):
+            print(ajedrez[i][j], end=" ")
+        print()
