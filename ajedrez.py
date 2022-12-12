c06e306cebbbe335e8b61517d7f60f04b3a2211e
Jacobo24 <jaccalgar@gmail.com>
2022-12-12 19:33:13 +0100

    fin de la prueba

--- ajedrez.py
+++ ajedrez.py
@@ -13,3 +13,12 @@ def imprimir_tablero(ajedrez):
         for j in range(8):
             print(ajedrez[i][j], end=" ")
         print()
+
+# Para que puedas mover las piezas
+def mover_pieza(ajedrez, pieza, fila, columna):
+    for i in range(8):
+        for j in range(8):
+            if ajedrez[i][j] == pieza:
+                ajedrez[i][j] = "  "
+                ajedrez[fila][columna] = pieza
+                return
